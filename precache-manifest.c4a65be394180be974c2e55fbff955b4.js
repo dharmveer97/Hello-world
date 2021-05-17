ec2bece19188e56d2509bca807657cf72106954d
dharmveer97 <dvbangar8@gmail.com>
2021-05-17 19:06:54 +0530

    Updates

---
+++ precache-manifest.c4a65be394180be974c2e55fbff955b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db2b387ed9091356e9e6f32a6f2196d",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "986dd51564ed811b2e2b",
+    "url": "/react-invoice-generator/static/css/2.9fca9f18.chunk.css"
+  },
+  {
+    "revision": "01f68087d54e8c1ee928",
+    "url": "/react-invoice-generator/static/css/main.02775c55.chunk.css"
+  },
+  {
+    "revision": "986dd51564ed811b2e2b",
+    "url": "/react-invoice-generator/static/js/2.94fbc6fd.chunk.js"
+  },
+  {
+    "revision": "8c65b9cc60f7a3841035f7df21a421d4",
+    "url": "/react-invoice-generator/static/js/2.94fbc6fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f68087d54e8c1ee928",
+    "url": "/react-invoice-generator/static/js/main.57c5cb5f.chunk.js"
+  },
+  {
+    "revision": "aabb303fe90f96d72bbb",
+    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+  }
+]);
